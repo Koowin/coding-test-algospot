efae8f2308fcdb27cd879305d51d232fe0606b3d
Koowin <56014482+Koowin@users.noreply.github.com>
2022-03-23 11:40:37 +0900

    solved: Fence

---
+++ src/Fence.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Fence {
+    public void solution() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("./input/fence");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream));
+
+        int caseCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i < caseCount; i++) {
+            br.readLine();
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            MaxRectangleFinder mrf = new MaxRectangleFinder(input);
+            System.out.println(mrf.getAnswer());
+        }
+    }
+
+    static class MaxRectangleFinder {
+        private final int[] fence;
+
+        private MaxRectangleFinder(int[] fence) {
+            this.fence = fence;
+        }
+
+        private int getAnswer(){
+            return getMaxRectangleSize(0, fence.length);
+        }
+
+        private int getMaxRectangleSize(int lo, int hi){
+            if((hi - lo) <= 1){
+                if(hi == lo){
+                    return 0;
+                }
+                return fence[lo];
+            }
+
+            int splitIndex = findMinIndex(lo, hi);
+            int leftMax = getMaxRectangleSize(lo, splitIndex);
+            int rightMax = getMaxRectangleSize(splitIndex+1, hi);
+            int midMax = fence[splitIndex] * (hi - lo);
+            int ret = Math.max(leftMax, rightMax);
+            ret = Math.max(ret, midMax);
+
+            return ret;
+        }
+
+        private int findMinIndex(int lo, int hi) {
+            int minHeight = Integer.MAX_VALUE;
+            int minIndex = 0;
+            for (int i = lo; i < hi; i++) {
+                if(minHeight > fence[i]){
+                    minHeight = fence[i];
+                    minIndex = i;
+                }
+            }
+            return minIndex;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new QuadTree().solution();
+        new FanMeeting().solution();
     }
 }
